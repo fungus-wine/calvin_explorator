9c2ff4b84dc0faa10d816bdb6938d44c671ec644
Damon <damoncali@gmail.com>
2026-02-13 11:37:16 -0600

    change loading background to dark

--- src/main/index.js
+++ src/main/index.js
@@ -8,6 +8,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    backgroundColor: '#252525',
     webPreferences: {
       preload: path.join(__dirname, '../preload/index.cjs'),
       contextIsolation: true,
